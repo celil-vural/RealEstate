594553c3790631140dfe376305d5eb9ef3fbb594
celil-vural <celilvural.work@gmail.com>
2023-12-09 16:31:19 +0300

    Created Empolyee pages and controller on UI

--- RealEstate_Dapper_WebApi/Utilities/Extensions/BuilderExtensions/ServiceExtensions.cs
+++ RealEstate_Dapper_WebApi/Utilities/Extensions/BuilderExtensions/ServiceExtensions.cs
@@ -14,32 +14,32 @@ using Repository.Repositories.TestimonialRepository;
 using Repository.Repositories.TestimonialRepsitory;
 using Repository.Repositories.WhoWeAreRepository;
 
-namespace RealEstate_Dapper_WebApi.Utilities.Extensions.BuilderExtensions
+namespace RealEstate_Dapper_WebApi.Utilities.Extensions.BuilderExtensions;
+
+public static class ServiceExtensions
 {
-    public static class ServiceExtensions
+    public static void ConfigureService(this IServiceCollection services)
     {
-        public static void ConfigureService(this IServiceCollection services)
-        {
-            services.ConfigureIoC();
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-        }
-        public static void ConfigureIoC(this IServiceCollection services)
-        {
-            services.AddTransient<DapperContext>();
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-            services.AddTransient<IProductRepository, ProductRepository>();
-            services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-            services.AddTransient<IServicesRepository, ServicesRepository>();
-            services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-            services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-            services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-            services.AddTransient<IContactRepository, ContactRepository>();
-            services.AddTransient<IAddressRepository, AddressRepository>();
-            services.AddTransient<IContactAddressRepository, ContactAddressRepository>();
-            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-            services.AddTransient<IProductShowCaseTypeRepository, ProductShowCaseTypeRepository>();
-        }
+        services.ConfigureIoC();
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public static void ConfigureIoC(this IServiceCollection services)
+    {
+        services.AddTransient<DapperContext>();
+        services.AddTransient<ICategoryRepository, CategoryRepository>();
+        services.AddTransient<IProductRepository, ProductRepository>();
+        services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+        services.AddTransient<IServicesRepository, ServicesRepository>();
+        services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+        services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+        services.AddTransient<ITestimonialRepository, TestimonialRepository>();
+        services.AddTransient<IContactRepository, ContactRepository>();
+        services.AddTransient<IAddressRepository, AddressRepository>();
+        services.AddTransient<IContactAddressRepository, ContactAddressRepository>();
+        services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+        services.AddTransient<IProductShowCaseTypeRepository, ProductShowCaseTypeRepository>();
     }
 }
--- RealEstate_UI/Controllers/EmployeeController.cs
+++ RealEstate_UI/Controllers/EmployeeController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Entity.Dtos.EmployeeDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -19,5 +20,59 @@ public class EmployeeController(IHttpClientFactory clientFactory) : Controller
         }
 
         return View();
+    }
+
+    [HttpGet]
+    public IActionResult CreateEmployee()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateEmployee(CreateEmployeeDto createEmployeeDto)
+    {
+        Console.WriteLine("------------------");
+        Console.WriteLine(createEmployeeDto.Phone);
+        Console.WriteLine(createEmployeeDto.Name);
+        var client = clientFactory.CreateClient();
+        var json = JsonConvert.SerializeObject(createEmployeeDto);
+        var data = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(Urls.EmployeeUrl, data);
+        if (response.IsSuccessStatusCode) return RedirectToAction("Index");
+        return View();
+    }
+
+    public async Task<IActionResult> DeleteEmployee(int id)
+    {
+        var client = clientFactory.CreateClient();
+        var response = await client.DeleteAsync($"{Urls.EmployeeUrl}/{id}");
+        if (response.IsSuccessStatusCode) return RedirectToAction("Index");
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateEmployee(int id)
+    {
+        var client = clientFactory.CreateClient();
+        var response = await client.GetAsync($"{Urls.EmployeeUrl}/{id}");
+        if (response.IsSuccessStatusCode)
+        {
+            var jsonData = await response.Content.ReadAsStringAsync();
+            var Employee = JsonConvert.DeserializeObject<UpdateEmployeeDto>(jsonData);
+            return View(Employee);
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateEmployee(UpdateEmployeeDto dto)
+    {
+        var client = clientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(dto);
+        var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var response = await client.PutAsync(Urls.EmployeeUrl, stringContent);
+        if (response.IsSuccessStatusCode) return RedirectToAction("Index");
+        return View(dto);
     }
 }
